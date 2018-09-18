cdfb27707997a237038f39c0626384dbd2574780
bhaveshs2408 <bhaveshs2408@gmail.com>
2018-09-18 20:51:48 +0530

    scraping

--- app/controllers/parallel_controller.rb
+++ app/controllers/parallel_controller.rb
@@ -3,10 +3,11 @@ class ParallelController < ApplicationController
   def parallelrate
     require 'openssl'
     require 'open-uri'
-    doc = Nokogiri::HTML(open(('https://www.abokifx.com/'), :ssl_verify_mode => OpenSSL::SSL::VERIFY_NONE))
-    entries = doc.css('.lagos-market-rates-inner')
-    rate = entries.css('table')[0].css('tr')[1].css('td')[1].text
-    @formattedrate = rate[6..8]
+    doc = Nokogiri::HTML(open('https://booking.village-hotels.co.uk/book/search/', :ssl_verify_mode => OpenSSL::SSL::VERIFY_NONE))
+    @entries = doc.css('.page-content').css('.container-inner').css('#skip').css("a").children.text
+    #subtitle
+    #rate = entries.css('table')[0].css('tr')[1].css('td')[1].text
+    #@formattedrate = rate[6..8]
     render template: 'parallel/home'
   end
 
